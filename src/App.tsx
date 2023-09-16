8348fab1d03b3dcdc2df03c55cec67793f7a86ed
Anne Rakesh <anne.rakesh2@globallogic.com>
2023-09-16 22:50:37 +0530

    initialized another state in App and also created the Model file with exported Interface todo[]

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./Components/InputField";
+import { Todo } from "./Components/Model";
 
 function App() {
+  let [todo, setTodo] = useState<string>("");
+  let [todos, setTodos] = useState<Todo[]>([]);
+
+  console.log(todo);
+
   return (
     <div className="App">
-      <span className="heading"> Taskify</span>
-      <InputField />
-
+      <span className="heading"> Task Entry</span>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 }
--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import "./Styles.css"
+import "./Styles.css";
 
-const InputField: React.FC = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
-      <input type="input" placeholder=" Enter a Task" className="input__box" />
+      <input
+        type="input"
+        placeholder="Enter a Task"
+        className="input__box"
+        value={todo}
+        onChange={(test) => setTodo(test.target.value)}
+      />
       <button type="submit" className="input__submit">
-        {" "}
-        Go{" "} 
+        Go
       </button>
     </form>
   );
