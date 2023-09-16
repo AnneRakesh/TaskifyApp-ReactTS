053689f4765cd98d0315f1451efc67bb498a5d37
Anne Rakesh <anne.rakesh2@globallogic.com>
2023-09-16 20:43:15 +0530

    I Practised few of my know logix in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  type Address = {
+    hno: string;
+    street: string;
+    pinCode: number;
+    test: [number, string];
+  };
+  type Person = {
+    personName: string;
+    age: number;
+    hobbies: string[];
+    address: Address;
+  };
 
+  let testAddress: Person = {
+    personName: "test",
+    age: 19,
+    hobbies: ["Cricket", "stockMarket"],
+    address: {
+      hno: " Colony",
+      street: "old Colony Street",
+      pinCode: 15,
+      test: [123, "Test"],
+    },
+  };
+
+  testAddress.personName = "John";
+
+  let isStudent: boolean;
+
+  isStudent = testAddress.age === 19;
+  console.log(isStudent);
 
   return (
     <div className="App">
