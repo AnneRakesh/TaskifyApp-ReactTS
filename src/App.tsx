c29d20c5c55a5be3846191b7793c6fa6da3e25bc
Anne Rakesh <anne.rakesh2@globallogic.com>
2023-09-16 23:07:16 +0530

    added handle add logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,22 @@ function App() {
   let [todo, setTodo] = useState<string>("");
   let [todos, setTodos] = useState<Todo[]>([]);
 
-  console.log(todo);
+  // console.log(todo);
+  console.log(todos);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), isDone: false, todo }]);
+    }
+    setTodo("")
+  };
 
   return (
     <div className="App">
       <span className="heading"> Task Entry</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 }
